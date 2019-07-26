ae3eff9973242b8076f373674d2c3077eee36218
Volosianskyi <o.volosianskyi@sloboda-studio.com>
2019-07-26 13:23:56 +0300

    added two pairs, fixed displaying, added two pairs test

--- game.rb
+++ game.rb
@@ -9,31 +9,37 @@ class Game
   end
 
   def pass_hand
-    fc = Card.new(@ranks, @suits) #create random card
-    @usercards.push(fc) #push it to user's cards array
+    fc = Card.new(@ranks, @suits) # create random card
+    @usercards.push(fc) # push it to user's cards array
     sc = fc
-    sc = Card.new(@ranks, @suits) while sc == fc #check if the 2nd card is the same as 1st
-    @usercards.push(sc) #if it's not the same, push it into user's array
+    sc = Card.new(@ranks, @suits) while sc == fc # check if the 2nd card is the same as the 1st
+    @usercards.push(sc) # if it's not the same, push it into user's array
   end
 
   def pass_table
+
     for i in 1..5 do
+      break if @tablecards.length == 5
       card = Card.new(@ranks, @suits)
-      @tablecards.push(card) unless exists(card) #to be shure that all cards are different
+      if !exists(card) # to be shure that all cards are different
+        @tablecards.push(card) 
+      else
+        pass_table
+      end
     end
   end
 
   def exists(card)
     @tablecards.each do |i|
-      return true if card.rank == i.rank && card.suit == i.suit #check the card in user's cards
+      return true if card.rank == i.rank && card.suit == i.suit # check the card in user's cards
     end
     @usercards.each do |i|
-      return true if card.rank == i.rank && card.suit == i.suit #check the card among cards on table
+      return true if card.rank == i.rank && card.suit == i.suit # check the card among cards on table
     end
     false
   end
 
-  def int_char(rank)
+  def int_char(rank) # turns number rank into char rank
     return 'A' if rank == 14
     return 'K' if rank == 13
     return 'Q' if rank == 12
@@ -41,7 +47,7 @@ class Game
     return rank if rank < 11
   end
 
-  def print1
+  def print1 # print cards
     @usercards.each do |i|
       print '|' + int_char(i.rank).to_s + i.suit + '|'
     end
@@ -51,7 +57,17 @@ class Game
     end
   end
 
-  def sort(arr)
+  def print2(cards, combo) # print cards with combo name
+    if cards.kind_of?(Array) 
+      print "\n It's " + combo + ': '
+      cards.each do |i|
+        print '|' + int_char(i.rank).to_s + i.suit + '|'
+      end
+      print "\n"
+    end
+  end
+
+  def sort(arr) # sort cards by rank
     j = 0;
     while j < arr.length
       j += 1
@@ -64,99 +80,154 @@ class Game
         end
       end
     end
-    print "\n"
-    
   end
 
-  def search_combo
+  def search_combo # container of other searching methods
     summary = @usercards + @tablecards
     sort(summary)
-    puts "\nPair? " + pair?(summary)[0].to_s + "\n"
-    puts 'Triple? ' + triple?(summary)[0].to_s + "\n"
-    puts 'Four of a kind? ' + four_of_a_kind?(summary)[0].to_s + "\n"
-    puts 'Fullhouse? ' + fullhouse?(summary).to_s + "\n"
-    puts 'Str8? ' + straight?(summary).to_s + "\n"
-    puts 'Flush? ' + flush?(summary)[0].to_s + "\n"
-    puts 'Str8 Flush? ' + straight_flush?(summary)[0].to_s + "\n"
 
+    pair = pair?(summary)
+    two_p = two_pairs?(summary)
+    set = triple?(summary)
+    four = four_of_a_kind?(summary)
+    fh = fullhouse?(summary)
+    straight = straight?(summary)
+    flush = flush?(summary)
+    str_flush = straight_flush?(summary)
+    royale_fl = royale_flush?(summary)
+
+    print2(pair[1], 'pair') if pair[0] 
+    print2(two_p[1], 'two pairs') if two_p[0] 
+    print2(set[1], 'set') if set[0] 
+    print2(four[1], 'four of a kind') if four[0] 
+    print2(fh, 'fullhouse') if fh
+    print2(straight[1], 'straight') if straight[0] 
+    print2(flush[1], 'flush') if flush[0] 
+    print2(str_flush[1], 'straight flush') if str_flush[0] 
+    print2(royale_fl[1], 'royale flush') if royale_fl[0] 
   end
 
-  def straight?(cards)
+  def straight?(cards) 
     sort(cards)
-    counter=0
-    res=[]
+    counter = 0
+    res = []
     cards.each do |i|
-      if cards.index(i) < cards.length - 1
-        if i.rank - cards[cards.index(i) + 1].rank == -1
-          counter += 1
-          res.push(i)
-          if cards.index(i) == cards.length - 2
-            res.push(cards[cards.index(i) + 1])
+      if cards.index(i) < cards.length - 1 # if index of card in array isn't last 
+        if i.rank - cards[cards.index(i) + 1].rank == -1 # check difference between this and next card
+          counter += 1 # if difference in ranks = -1
+          res.push(i) # add this card into array
+          if cards.index(i) == cards.length - 2 # if the card's penultimate 
+            res.push(cards[cards.index(i) + 1]) # then add the last card as well
           end
         end
       end
     end
-    
-    if counter > 4
-      if res.length > 5
+
+    if counter > 4 # if we have streak of cards >4
+      if res.length > 5 # if the streak is greater then 5
         res.each do |j|
           res.each do |i|
-            if res.index(i) < res.length - 5
-              res.delete(i) 
+            if res.index(i) < res.length - 5 # leave only the last five cards (because they have higher rank)
+              res.delete(i) # remove fewer ranks from array
             end
           end
         end
       end
-      return [true, res] 
+      return [true, res] # return array of true and array of cards
     end
 
-    [false, 0]
+    [false, 0] # if it's not straight 
   end
 
   def pair?(cards)
     sort(cards)
+    state = false
     result = []
     cards.each do |i|
-      if cards.index(i) < cards.length-1
-        result.push(true, i.rank) if i.rank == cards[cards.index(i) + 1].rank
-        else
-          result.push(false, 0)
+      if cards.index(i) < cards.length - 1
+        if i.rank == cards[cards.index(i) + 1].rank 
+          state = true
+          result.push(i, cards[cards.index(i) + 1]) # push this card and the next card (if heir ranks are the same)
+          res = [state, result]
+          return res
+        end
+      else
+        result.push(0)
       end
     end
-    result
+    res = [false,0]
+    res
+  end
+
+  def two_pairs?(cards)
+    one_pair = pair?(cards) # check does it have one pair
+    if one_pair[0]
+      state = false
+      result = []
+      cards.each do |i|
+        if cards.index(i) < cards.length - 1
+          if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank != one_pair[1][0].rank) # repeat the same as in 'pair?' but the ranks must differ
+            state = true
+            result.push(i, cards[cards.index(i) + 1])
+            res = [state, one_pair[1] + result]
+            return res
+          end
+        else
+          result.push(0)
+        end
+      end
+      res = [false, 0]
+      return res
+    end
+    res = [false, 0]
+    res
   end
 
   def triple?(cards)
+    state = false
     sort(cards)
     result = []
     cards.each do |i|
       if cards.index(i) < cards.length - 2
-        result.push(true, i.rank) if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank == cards[cards.index(i) + 2].rank)
-        else
-          result.push(false, 0)
+        if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank == cards[cards.index(i) + 2].rank) # if three cards in a row are the same
+          state = true
+          result.push(i,cards[cards.index(i) + 1],cards[cards.index(i) + 2]) # push into array this three cards
+          res = [state, result] 
+          return res
+        end
+      else
+        result.push(0)
       end
     end
-    result
+    res = [false, 0]
+    res
   end
 
-  def four_of_a_kind?(cards)
+  def four_of_a_kind?(cards) # the same as 'triple?' but 'if' is longer :D
+    state = false
     result = []
     cards.each do |i|
       if cards.index(i) < cards.length - 2
-        result.push(true, i.rank) if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank == cards[cards.index(i) + 2].rank) && (i.rank == cards[cards.index(i) + 3].rank)
-         else
-           result.push(false, 0)
+        if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank == cards[cards.index(i) + 2].rank) && (i.rank == cards[cards.index(i) + 3].rank)
+          state = true
+          result.push(i, cards[cards.index(i) + 1], cards[cards.index(i) + 2], cards[cards.index(i) + 3]) 
+          res = [state, result]
+          return res
+        end
+      else
+        result.push(0)
       end
     end
-    result
+    res = [false, 0]
+    res
   end
 
   def fullhouse?(cards)
-    trip = triple?(cards)
-    if trip[0] == true
+    trip = triple?(cards) # check do cards have set
+    if trip[0] 
       cards.each do |i|
         if cards.index(i) < cards.length-1
-          if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank != trip[1])
+          if (i.rank == cards[cards.index(i) + 1].rank) && (i.rank != trip[1]) # if they have set of 3 cards, then search for pair with different rank
             return true
           end
         end
@@ -169,13 +240,13 @@ class Game
 
   def flush?(cards)
     result = []
-    flag=false
-    s=0
-    c=0
-    h=0
-    d=0
+    flag = false
+    s = 0
+    c = 0
+    h = 0
+    d = 0
     cards.each do |i|
-      case i.suit
+      case i.suit # check the suit of card and increment variable 
       when '♠'
         s += 1
       when '♣'
@@ -184,23 +255,23 @@ class Game
         d += 1
       when '♥'
         h += 1
-      end  
+      end
     end
-    if s >= 5
-      result = find_suit(cards, '♠') 
-      flag=true
+    if s >= 5 # if we have five or greater cards with the same suit
+      result = find_suit(cards, '♠') # see description of 'find_suit' below
+      return [true, result]
     end
     if c >= 5
-      result = find_suit(cards, '♣') -
-      flag=true
+      result = find_suit(cards, '♣')
+      return [true, result]
     end
     if d >= 5
-      result = find_suit(cards, '♦') 
-      flag=true
+      result = find_suit(cards, '♦')
+      return [true, result]
     end
     if h >= 5
-      result = find_suit(cards, '♥') 
-      flag=true
+      result = find_suit(cards, '♥')
+      return [true, result]
     end
 =begin if result.length > 5
       result.each do |j|
@@ -212,15 +283,11 @@ class Game
       end
     end
 =end 
-    if flag == true
-      final = [true, result]
-    else
-      final=[false,0]
-    end
+    final = [false, 0]
     final
   end
 
-  def find_suit(cards, suit1)
+  def find_suit(cards, suit1) # find all cards with the same suit and return an array of them
     result = []
     cards.each do |i|
       result.push(i) if i.suit == suit1
@@ -230,22 +297,19 @@ class Game
   end
 
   def straight_flush?(cards)
-    is_flush = flush?(cards)
-    if is_flush[0] == true
-      str8 = straight?(is_flush[1])
-      if str8[0] == true
-        return str8
-      end
+    is_flush = flush?(cards) 
+    if is_flush[0] # if it's flush
+      str8 = straight?(is_flush[1]) 
+      return str8 if str8[0] # and if it's straight then it's straight flush :)
     else
-      return [false,0]
+      return [false, 0]
     end
   end
 
   def royale_flush?(cards)
-    str8_fl = straight_flush?(cards)
-    if str8_fl[0] == true
-      puts "\n RF GOT REQUEST: " + str8_fl[1][0].rank.to_s
-      if (str8_fl[1][0].rank == 10) && (str8_fl[1][4].rank == 14)
+    str8_fl = straight_flush?(cards) # check if it's str8 flush
+    if str8_fl[0] 
+      if (str8_fl[1][0].rank == 10) && (str8_fl[1][4].rank == 14) # if cards are between '10' and 'A'
         return str8_fl
       else
         return [false, ['ERR1']]
@@ -255,7 +319,6 @@ class Game
     end
   end
 end
-
 
 g = Game.new
 g.pass_hand
--- tests.rb
+++ tests.rb
@@ -2,6 +2,7 @@ require_relative 'card'
 require_relative 'game'
 
 class Tests
+
   def test_fullhouse
     c1 = Card.new([2], ['♠'])
     c2= Card.new([2], ['♠'])
@@ -18,7 +19,7 @@ class Tests
     c3.rank = 2
     c4.suit = '♠'
     c4.rank = 3
-    c5.suit = '♦'
+    c5.suit = '♠'
     c5.rank = 3
     c6.suit = '♦'
     c6.rank = 6
@@ -26,7 +27,38 @@ class Tests
     c7.rank = 7
     cards = [c1, c2, c3, c4, c5, c6, c7]
     g = Game.new
-    puts "_________________\n\nFullhouse test: " + g.fullhouse?(cards).to_s + "\n_________________\n\n"
+    puts "_________________\n\nTwo pairs test: " + g.two_pairs?(cards).to_s + "\n_________________\n\n"
+  end
+
+  def test_two_pairs
+    c1 = Card.new([2], ['♠'])
+    c2= Card.new([2], ['♠'])
+    c3= Card.new([2], ['♠'])
+    c4= Card.new([2], ['♠'])
+    c5= Card.new([2], ['♠'])
+    c6= Card.new([2], ['♠'])
+    c7= Card.new([2], ['♠'])
+    c1.suit = '♠'
+    c1.rank = 2
+    c2.suit = '♣'
+    c2.rank = 2
+    c3.suit = '♦'
+    c3.rank = 8
+    c4.suit = '♠'
+    c4.rank = 3
+    c5.suit = '♦'
+    c5.rank = 3
+    c6.suit = '♣'
+    c6.rank = 6
+    c7.suit = '♦'
+    c7.rank = 7
+    cards = [c1, c2, c3, c4, c5, c6, c7]
+    g = Game.new
+    print "_________________\n\nTwo pairs test: " 
+    g.two_pairs?(cards)[1].each do |i|
+      print '|' + g.int_char(i.rank).to_s + i.suit + '|'
+    end
+    print "\n_________________"  
   end
   
   def test_straight
@@ -157,6 +189,7 @@ class Tests
 end
 
 t = Tests.new
+t.test_two_pairs
 t.test_flush
 t.test_fullhouse
 t.test_straight
